603a21daddbe1b276205a4f467c6609224723566
Masaki Kaminaga <ma_kaminaga@bbenterprise.jp>
2021-12-04 08:14:19 +0900

    画面遷移　URL入力

--- htdocs/common/js/kirinuki_ui.js
+++ htdocs/common/js/kirinuki_ui.js
@@ -1,19 +1,31 @@
 //画面操作UIのスクリプト
 
-////////////モーダル
-const modalList = [
-  { trigger:'video_pray', url:'video_pray.html #modal > *' }
+////////////画面
+const pageList = [
+  { trigger:'input_url', url:'PC-Top_inputURL.html .screen.input_url > *' } //URL入力画面
 
 ]
 
 
+////////////モーダル
+const modalList = [
+  { trigger:'video_pray', url:'video_pray.html #modal > *' }
+]
+
+
 const PageUI = {
+  pageOpen: function(trigger,url){
+    var triggerDom = $('[data-page-trigger=' + trigger + ']');
+      $('.screen').empty().removeClass().addClass('screen input_url').load(url);
+  }
+
+  ,
+
   modalOpen: function(trigger,url){
-    var triggerDom = $('[data-modalOpener=' + trigger + ']');
-    triggerDom.on('click',function(){
+    var triggerDom = $('[data-modal-opener=' + trigger + ']');
       $('#modal_overlay').addClass('active');
       $('#modal').addClass('modal active').addClass(trigger).load(url);
-    });
+
     $('#modal_overlay').on('click',function(){
       $('#modal_overlay').removeClass('active');
       $('#modal').removeClass();
@@ -21,7 +33,16 @@ const PageUI = {
   }
 }
 
-PageUI.modalOpen(modalList[0].trigger, modalList[0].url);
+$('[data-modal-opener],[data-page-trigger]').on('click',function(){
+  var listNum = $(this).data('page-url');
+
+  if($(this).data('modal-opener')){
+    PageUI.modalOpen(modalList[listNum].trigger, modalList[listNum].url);
+
+  } else if ($(this).data('page-trigger')){
+    PageUI.pageOpen(pageList[listNum].trigger, pageList[listNum].url);
+  }
+});
 
 
 
